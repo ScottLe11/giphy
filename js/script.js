2fd63158fb3ef279bec9b743f30a4649fbbeb932
Glitch (charm-tarry-arthropod) <none>
2021-05-04 00:37:27 +0000

    ☂️🖖 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 /* global $ */
 
 $(".search-button").click(function(){
-  
-
+  let giphyApi = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY"
+  fetch(giphyApi)
+  .then(function(response) {
+    return response.json()
+  })
+    
+       .then(function(data){
+          console.log(data)
+  })     
   
 });
 
